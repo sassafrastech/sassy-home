b38aaf06403fda82103953bfb0109f60e3c6f388
Elizabeth Mitchell <elizabeth@sassafras.coop>
2015-12-14 13:44:33 -0500

    1857: Add reply to address to inquiry template

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -5,7 +5,9 @@ class NotificationsMailer < ActionMailer::Base
   def new_message(message)
     @message = message
     subject = "Inquiry from " + message.name.to_s
+    reply_address = message.email.to_s
     mail(:subject => subject)
+    mail(:reply_to => reply_address)
   end
 
 end
