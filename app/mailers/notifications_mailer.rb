5f660e399b9be2ae3ce80b68892798a27d344215
Elizabeth Mitchell <elizabeth@sassafras.coop>
2015-12-14 13:28:55 -0500

    2652: Include name of person in email inquiry

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,10 +1,11 @@
 class NotificationsMailer < ActionMailer::Base
   default :from => "info@sassafras.coop"
-  default :to => ["info@sassafras.coop"]
+  default :to => "info@sassafras.coop"
 
   def new_message(message)
     @message = message
-    mail(:subject => "Inquiry")
+    subject = "Inquiry from " + message.name.to_s
+    mail(:subject => subject)
   end
 
 end
