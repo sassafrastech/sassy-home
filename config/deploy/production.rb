0f8b0fd8273cce50e4d67c0ec503c1795a21600e
Elizabeth Mitchell <elizabeth@sassafras.coop>
2017-04-19 06:27:22 -0400

    Update path

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,6 @@ role :app, %w{tomsmyth@sassafras.coop}
 role :web, %w{tomsmyth@sassafras.coop}
 role :db,  %w{tomsmyth@sassafras.coop}
 set :rails_env, 'production'
-set :branch, 'master'
-set :bundle_path, -> { "/home/tomsmyth/webapps/rails/gems" }
-set :bundle_binstubs, -> { "/home/tomsmyth/webapps/rails/bin" }
+set :branch, 'migration'
+set :bundle_path, -> { "/home/tomsmyth/ror/rails/gems" }
+set :bundle_binstubs, -> { "/home/tomsmyth/ror/rails/bin" }
