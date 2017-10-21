0e481796924a933e28bdb4bf92edef3095a49d15
Tom Smyth <tom@sassafras.coop>
2017-10-21 15:34:09 -0400

    Changed deploy paths for server migration

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
-role :app, %w{tomsmyth@sassafras.coop}
-role :web, %w{tomsmyth@sassafras.coop}
-role :db,  %w{tomsmyth@sassafras.coop}
+role :app, %w{sassafras@sassafras.coop}
+role :web, %w{sassafras@sassafras.coop}
+role :db,  %w{sassafras@sassafras.coop}
 set :rails_env, 'production'
 set :branch, 'master'
-set :bundle_path, -> { "/home/tomsmyth/webapps/ror/gems" }
-set :bundle_binstubs, -> { "/home/tomsmyth/webapps/ror/bin" }
+set :bundle_path, -> { "/home/sassafras/webapps/ror/gems" }
+set :bundle_binstubs, -> { "/home/sassafras/webapps/ror/bin" }
