3bdaf4c35d263bdd35e8acc4b7027b25ea5310c9
Kevin Cooper <cooperka92@gmail.com>
2020-09-08 18:09:18 -0700

    Update main branch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,6 @@ role :app, %w{sassafras@sassafras.coop}
 role :web, %w{sassafras@sassafras.coop}
 role :db,  %w{sassafras@sassafras.coop}
 set :rails_env, 'production'
-set :branch, 'master'
+set :branch, 'main'
 set :bundle_path, -> { "/home/sassafras/webapps/ror/gems" }
 set :bundle_binstubs, -> { "/home/sassafras/webapps/ror/bin" }
