70d4749e702ccc08d3f4570fe6b730877b04a599
Kevin Cooper <cooperka92@gmail.com>
2020-09-10 14:06:21 -0700

    Add PDF with friendly link

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ SassSite::Application.routes.draw do
   # Jobs
   get "/jobs/project-planner", controller: :jobs, to: 'jobs#project_planner', as: "project_planner"
   get "/jobs/rails-developer", controller: :jobs, to: 'jobs#rails_developer', as: "rails_developer"
+  get "/jobs/product-planning-lead-2020", :to => redirect("/sassafras-product-planning-lead-2020.pdf")
 
   # Legacy link support
   get "/our_work" => redirect("/our-work")
